4e23368a9156a98eba1056b7acc1fda88a0d2a7f
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-09 23:04:18 +0300

    create checkout session view

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -8,4 +8,9 @@ app_name = "subscriptions"
 urlpatterns = [
     path('success/', views.SuccessView.as_view(), name='success'),
     path('cancel/', views.CancelView.as_view(), name='cancel'),
+    path(
+        'checkout/', 
+        views.CreateCheckoutSessionView.as_view(), 
+        name="checkout"
+    )
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,6 +1,12 @@
+import stripe
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.shortcuts import redirect
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class SuccessView(LoginRequiredMixin, TemplateView):
@@ -9,3 +15,26 @@ class SuccessView(LoginRequiredMixin, TemplateView):
 
 class CancelView(LoginRequiredMixin, TemplateView):
     template_name = "subscriptions/cancel.html"
+
+
+class CreateCheckoutSessionView(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            customer_email=request.user.email,
+            client_reference_id=request.user.id,
+            line_items=[
+                {
+                    'price': settings.STRIPE_PRICE_ID,
+                    'quantity': 1,
+                },
+            ],
+            mode='subscription',
+            subscription_data={
+                "trial_period_days": settings.STRIPE_TRIAL_PERIOD_DAYS
+            },
+            success_url=settings.DOMAIN_URL + '/success/',
+            cancel_url=settings.DOMAIN_URL + '/cancel/',
+        )
+        return redirect(checkout_session.url)
