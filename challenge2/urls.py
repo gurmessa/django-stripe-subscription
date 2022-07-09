d9c24f4f590bbd158565c3855beda93e50e91d3e
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-09 21:40:11 +0300

    setup django_registration urls

--- challenge2/urls.py
+++ challenge2/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
+from accounts.forms import UserRegistrationForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'accounts/register/',
+        RegistrationView.as_view(
+            form_class=UserRegistrationForm
+        ),
+        name='django_registration_register',
+    ),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
