9b72c83866055f0b7d7685919d86b760216b6f69
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-09 21:30:40 +0300

    install and setup crispy form

--- challenge2/settings.py
+++ challenge2/settings.py
@@ -12,13 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import environ
 import os
+from pathlib import Path
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'crispy_forms',
+    
     'accounts',
 ]
 
@@ -134,3 +135,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
