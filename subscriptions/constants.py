afc9ba5dce4241f0c703bce0a52306d0dbe260cc
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-09 23:09:29 +0300

    create constants for stripe status

---
+++ subscriptions/constants.py
@@ -0,0 +1,19 @@
+SUBSCRIPTION_STATUES = [
+    "active",
+    "past_due",
+    "unpaid",
+    "canceled",
+    "incomplete",
+    "incomplete_expired",
+    "trialing"
+]
+
+SUBSCRIPTION_CHOICES = [
+    ("active", "Active"),
+    ("past_due", "Past Due"),
+    ("unpaid", "Unpaid"),
+    ("canceled", "Canceled"),
+    ("incomplete", "Incomplete"),
+    ("incomplete_expired", "Incomplete_expired"),
+    ("trialing", "Trialing")
+]
