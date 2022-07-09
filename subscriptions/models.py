cc057ff9944e13512722bd54785ee5e8b4c67b06
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-09 23:09:42 +0300

    create subscription model

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from .constants import SUBSCRIPTION_CHOICES
 
-# Create your models here.
+
+class Subscription(models.Model):
+    user = models.OneToOneField(to=get_user_model(), on_delete=models.CASCADE)
+    subscription_id = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, choices=SUBSCRIPTION_CHOICES)
+    start_date = models.DateTimeField()
+    ended_at = models.DateTimeField(null=True, blank=True)
+    trial_end = models.DateTimeField()
+    trial_start = models.DateTimeField()
+    canceled_at = models.DateTimeField(null=True, blank=True)
+    current_period_end = models.DateTimeField()
+    current_period_start = models.DateTimeField()
