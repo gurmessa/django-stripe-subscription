aabadd3320fb9560116a746587dea0b8afea2b2d
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-10 13:51:43 +0300

    test success page

--- subscriptions/tests/test_views.py
+++ subscriptions/tests/test_views.py
@@ -29,6 +29,31 @@ class HomeViewTest(TestCase):
         self.assertTemplateUsed(response, 'subscriptions/home.html')
 
 
+class SuccessView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # create user
+        User = get_user_model()
+        User.objects.create_user(
+            username="test",
+            email='test@user.com', 
+            password='foo')
+
+    def test_view_redirects_if_not_authorized(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+         
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username='test', password='foo')
+        response = self.client.get('/success/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_success_page_returns_correct_html(self):
+        self.client.login(username='test', password='foo')
+        response = self.client.get('/success/')
+        self.assertTemplateUsed(response, 'subscriptions/success.html')
+
+
 class CancelViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -48,7 +73,8 @@ class CancelViewTest(TestCase):
         response = self.client.get('/cancel/')
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_returns_correct_html(self):
+    def test_cancel_page_returns_correct_html(self):
         self.client.login(username='test', password='foo')
         response = self.client.get('/cancel/')
         self.assertTemplateUsed(response, 'subscriptions/cancel.html')
+
