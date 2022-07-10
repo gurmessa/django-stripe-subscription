31f5776ddf970d4a7e1ec277bb2c9592f8816cca
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-10 13:50:25 +0300

    test cancel page

--- subscriptions/tests/test_views.py
+++ subscriptions/tests/test_views.py
@@ -27,3 +27,28 @@ class HomeViewTest(TestCase):
         self.client.login(username='test', password='foo')
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'subscriptions/home.html')
+
+
+class CancelViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # create user
+        User = get_user_model()
+        User.objects.create_user(
+            username="test",
+            email='test@user.com', 
+            password='foo')
+
+    def test_view_redirects_if_not_authorized(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+         
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username='test', password='foo')
+        response = self.client.get('/cancel/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_returns_correct_html(self):
+        self.client.login(username='test', password='foo')
+        response = self.client.get('/cancel/')
+        self.assertTemplateUsed(response, 'subscriptions/cancel.html')
