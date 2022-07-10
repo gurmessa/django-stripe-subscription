eb97ee6f088385f6a94f72467a00b4c420b64280
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-10 14:06:07 +0300

    test subscription view

--- subscriptions/tests/test_views.py
+++ subscriptions/tests/test_views.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta
 from django.test import TestCase
 from django.urls import reverse
 from django.test import Client
 from django.contrib.auth import get_user_model
+from django.conf import settings
+from subscriptions.models import Subscription
 
 
 class HomeViewTest(TestCase):
@@ -78,3 +81,54 @@ class CancelViewTest(TestCase):
         response = self.client.get('/cancel/')
         self.assertTemplateUsed(response, 'subscriptions/cancel.html')
 
+
+class SubscriptionView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User = get_user_model()
+
+        # create a user that has no stripe subcription
+        user1 = User.objects.create_user(
+            username="test",
+            email='test@user.com', 
+            password='foo')
+
+        # create a user that has stripe subcription
+        user2 = User.objects.create_user(
+            username="test2",
+            email='test2@user.com', 
+            password='foo')
+
+        current_date = datetime.now()
+        trial_end_date = current_date + \
+            timedelta(days=settings.STRIPE_TRIAL_PERIOD_DAYS)
+        # create subscription for user 2
+        Subscription.objects.create(
+            status="trialing",
+            user=user2,
+            start_date=current_date,
+            trial_start=current_date,
+            trial_end=trial_end_date,
+            current_period_start=current_date,
+            current_period_end=trial_end_date
+        )
+
+    def test_view_redirects_if_not_authorized(self):
+        response = self.client.get('/subscription/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_redirects_to_home_if_not_subscribed(self):
+        self.client.login(username='test', password='foo')
+        response = self.client.get('/subscription/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/')
+
+    def test_view_url_exists_at_desired_location_for_subscribed_user(self):
+        self.client.login(username='test2', password='foo')
+        response = self.client.get('/subscription/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_subscription_page_returns_correct_html(self):
+        self.client.login(username='test2', password='foo')
+        response = self.client.get('/subscription/')
+        self.assertTemplateUsed(response, 'subscriptions/subscription.html')
