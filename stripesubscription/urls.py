1e01225445c0de443c6c12b3c485bd95e0c2baaa
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-07-26 11:57:34 +0300

    rename project to stripesubscription

--- stripesubscription/urls.py
+++ stripesubscription/urls.py
@@ -1,4 +1,4 @@
-"""challenge2 URL Configuration
+"""stripesubscription URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
